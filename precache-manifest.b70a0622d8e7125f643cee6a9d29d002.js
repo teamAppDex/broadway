2cd971c7d4f08972492eb664a544c0e0094a751d
Application Design <my.appdex@gmail.com>
2020-04-04 19:37:50 +0100

    Updates

---
+++ precache-manifest.b70a0622d8e7125f643cee6a9d29d002.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "046b24da45a2eb1ba75a03aff1782663",
+    "url": "/broadway/index.html"
+  },
+  {
+    "revision": "3e6caefe40d6c59ea8e8",
+    "url": "/broadway/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "91286bc0db2e729fbea4",
+    "url": "/broadway/static/js/2.f616c302.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/broadway/static/js/2.f616c302.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e6caefe40d6c59ea8e8",
+    "url": "/broadway/static/js/main.5b83f777.chunk.js"
+  },
+  {
+    "revision": "33216982967af16d2e4e",
+    "url": "/broadway/static/js/runtime-main.15272dd1.js"
+  }
+]);
